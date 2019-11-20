d26ec4b0bd4801732dbdbbc544cfbceb83df39cc
Alexis <reche06@gmail.com>
2019-11-20 18:29:00 -0500

    intial commit

---
+++ src/footer.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Footer(){
+    return <h4 style={{color: "Black", fontSize: "1rem"}}> © Made by: Alexis Hill</h4>;
+}
---
+++ src/header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Header(){
+    return <h1 style={{color: "Black", fontSize: "3rem"}}>Nasa Photo Of The Day</h1>;
+};
+
+export default Header;
